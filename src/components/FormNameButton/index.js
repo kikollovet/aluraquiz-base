215c4cbb54c270a672bbc461e2d288301e804554
Kiko <kikollovet@gmail.com>
2021-01-26 15:52:15 -0300

    Botao cor diferente disabled

--- src/components/FormNameButton/index.js
+++ src/components/FormNameButton/index.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const FormNameButton = styled.button`
-  background-color: #F1A74E; /* Green */
+  background-color: #EE982F; /* Green */
   border: none;
   color: white;
   margin-top: 15px;
@@ -11,6 +11,9 @@ const FormNameButton = styled.button`
   /*display: inline-block;*/
   word-break: break-word;
   font-size: 16px;
+  :disabled {
+    background-color: #F1A74E;
+  }
   
 `
 
